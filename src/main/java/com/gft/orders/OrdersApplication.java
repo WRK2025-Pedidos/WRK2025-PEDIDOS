c9c014db6f0a76e8c2e431ea6cff4b2477e61abb
lorenus-dev <lorena.sanchis@gft.com>
2025-05-14 11:25:41 +0200

    Refactor: rename package pedidos → order

--- src/main/java/com/gft/orders/OrdersApplication.java
+++ src/main/java/com/gft/orders/OrdersApplication.java
@@ -1,13 +1,13 @@
-package com.gft.pedidos;
+package com.gft.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PedidosApplication {
+public class OrdersApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PedidosApplication.class, args);
+        SpringApplication.run(OrdersApplication.class, args);
     }
 
 }
