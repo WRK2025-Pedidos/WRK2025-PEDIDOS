b2e38641f5718618a1b0f85d56497f16cc595403
lorenus-dev <lorena.sanchis@gft.com>
2025-05-14 11:36:52 +0200

    Tdd/create order line class (#9)
    * Prueba de push
    
    * TDD Order Class constructor green
    
    # Conflicts:
    #	src/test/java/com/gft/pedidos/PedidosApplicationTests.java
    
    * Refactor pedidos-> orders
    
     Conflicts:
    	src/test/java/com/gft/orders/PedidosApplicationTests.java
    
    * no args constructor test green
    
    * setter product test green
    
    * setter quantity test green
    
    * setter refund test green
    
    * equals  test green
    
    * Eliminación archivo innecesario
    
    * Trigger conflict state refresh in GitHub
    
    ---------
    
    Co-authored-by: Kathy <catherine.alventos@gft.com>

---
+++ src/main/java/com/gft/orders/domain/model/OrderLines.java
@@ -0,0 +1,14 @@
+package com.gft.orders.domain.model;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class OrderLines {
+
+  UUID product;
+  int quantity;
+  Boolean refund;
+
+}
