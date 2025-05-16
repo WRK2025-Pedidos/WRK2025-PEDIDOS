c6db093b68fbf68d04413147bd12b269be579fa6
lorenus-dev <lorena.sanchis@gft.com>
2025-05-16 12:51:53 +0200

    Fix merge conflicts (#27)
    * W2025ORDER-15: configuration
    
    * W2025ORDER-15: JPA + Schema corrections
    
    * W2025ORDER-15: modificacion h2 en properties
    
    * W2025ORDER-15: modificacion hibernate properties
    
    * W2025ORDER-15: añadir anotaciones @Generated
    
    * W2025ORDER-15: modificacion configuracion jacoco
    
    * cambio

--- src/main/java/com/gft/orders/domain/model/entity/OrderReturn.java
+++ src/main/java/com/gft/orders/domain/model/entity/OrderReturn.java
@@ -4,7 +4,6 @@ import com.gft.orders.domain.model.valueObject.OrderLines;
 import lombok.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
