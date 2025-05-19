1a496c631bbf31a283cdde279cff2024ab4437a9
lorenus-dev <lorena.sanchis@gft.com>
2025-05-19 11:22:17 +0200

    W2025ORDER-19: order service created

---
+++ src/main/java/com/gft/orders/application/dto/OrderDTO.java
@@ -0,0 +1,10 @@
+package com.gft.orders.application.dto;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public record OrderDTO( UUID id
+
+
+) implements Serializable {
+}
---
+++ src/main/java/com/gft/orders/application/service/OrderServices.java
@@ -0,0 +1,16 @@
+package com.gft.orders.application.service;
+
+import com.gft.orders.domain.model.entity.Order;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrderServices {
+
+    UUID createOrder(Order order);
+
+    Optional<Order> findOrderById(UUID id);
+
+    List<Order> findAllOrders();
+}
