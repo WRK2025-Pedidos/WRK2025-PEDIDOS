65bb4d12af5fe4333daa7259f6b550a6786ca843
Kathy <catherine.alventos@gft.com>
2025-05-20 11:35:12 +0200

    W2025ORDER-24: Create ExceptionsGeneralHandler TDD red

--- src/main/java/com/gft/orders/domain/exceptions/ExceptionsGeneralHandler.java
+++ src/main/java/com/gft/orders/domain/exceptions/ExceptionsGeneralHandler.java
@@ -1,5 +1,63 @@
 package com.gft.orders.domain.exceptions;
 
-public class ExceptionsGeneralHandler {
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionsGeneralHandler extends ResponseEntityExceptionHandler {
+//
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<Object> handleAllExceptions(Exception ex) {
+//
+//        logger.error("Unexpected error", ex);
+//
+//        return ResponseEntity.internalServerError().body(new ErrorResponse("Something went wrong"));
+//    }
+//
+//    @ExceptionHandler(BusinessExceptions.class)
+//    public ResponseEntity<Object> handleBusinessExceptions(BusinessExceptions ex) {
+//
+//        logger.error("JSON object cannot be deserialized");
+//
+//        return ResponseEntity.internalServerError().body(new ErrorResponse(ex.getMessage()));
+//    }
+//
+//    // **********************************************************************************************
+//
+//    @Override
+//    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+//
+//        logger.error("HttpMessageNotReadableException is thrown");
+//
+//        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+//    }
+//
+//    @Override
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+//
+//        ErrorResponse errorResponse = new ErrorResponse("Unable to validate request parameters");
+//
+//        return handleExceptionInternal(ex, errorResponse, headers, HttpStatus.BAD_REQUEST, request);
+//    }
+//
+//    @Override
+//    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+//
+//        String invalidParameter = ex.getPropertyName();
+//        Object invalidValue = ex.getValue();
+//        String typeExpected = != null ? requiredType.getSimpleName() : null;
+//
+//        if (typeExpected == null) {
+//            throw new NullPointerException("Type of required property '" + invalidParameter + "' is null");
+//        }
+//
+//        ErrorResponse errorResponse = new ErrorResponse("Invalid type of required property '" + invalidParameter + "' returns '" + invalidValue + "'. The following type was expected: '" + typeExpected + "'.");
+//
+//        return handleExceptionInternal(ex, errorResponse, headers, HttpStatus.BAD_REQUEST, request);
+//    }
 
 }
--- src/test/java/com/gft/orders/domain/exceptions/ExceptionsGeneralHandlerTest.java
+++ src/test/java/com/gft/orders/domain/exceptions/ExceptionsGeneralHandlerTest.java
@@ -1,4 +1,86 @@
 package com.gft.orders.domain.exceptions;
 
+import org.hibernate.TypeMismatchException;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ControllerAdvice
 public class ExceptionsGeneralHandlerTest {
+
+    ExceptionsGeneralHandlerTest handler = new ExceptionsGeneralHandlerTest();
+
+    @Test
+    void handleGenericException_ShouldReturnInternalServerError() {
+        Exception ex = new Exception("Unexpected error");
+        ResponseEntity<Object> response = handler.handleAllExceptions(ex);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertTrue(((ErrorResponse) response.getBody()).getError().contains("Something went wrong"));
+    }
+
+
+    @Test
+    void handleBusinessException_ShouldReturnInternalServerError() {
+        BusinessExceptions ex = new BusinessExceptions("Domain error");
+        ResponseEntity<Object> response = handler.handleBusinessExceptions(ex);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals("Domain error", ((ErrorResponse) response.getBody()).getError());
+    }
+
+    @Test
+    void handleHttpMessageNotReadable_ShouldReturnBadRequest() {
+        HttpMessageNotReadableException ex = new HttpMessageNotReadableException("Malformed JSON", (HttpInputMessage) null);
+        WebRequest request = mock(WebRequest.class);
+        ResponseEntity<Object> response = handler.handleHttpMessageNotReadable(ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Malformed JSON", response.getBody());
+    }
+
+    @Test
+    void handleMethodArgumentNotValid_ShouldReturnBadRequest() {
+        BindingResult bindingResult = new BeanPropertyBindingResult(new Object(), "object");
+        MethodParameter param = mock(MethodParameter.class);
+        MethodArgumentNotValidException ex = new MethodArgumentNotValidException(param, bindingResult);
+        WebRequest request = mock(WebRequest.class);
+        ResponseEntity<Object> response = handler.handleMethodArgumentNotValid(ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertTrue(((ErrorResponse) response.getBody()).getError().contains("Unable to validate request parameters"));
+    }
+
+    @Test
+    void handleTypeMismatch_ShouldReturnBadRequest() {
+        TypeMismatchException ex = new TypeMismatchException("abc", Integer.class);
+        WebRequest request = mock(WebRequest.class);
+        ex.initPropertyName("quantity");
+        ResponseEntity<Object> response = handler.handleTypeMismatch(ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertTrue(((ErrorResponse) response.getBody()).getError().contains("Invalid type"));
+    }
+
+    @Test
+    void handleTypeMismatch_ShouldThrowNullPointerException_WhenRequiredTypeIsNull() {
+        TypeMismatchException ex = mock(TypeMismatchException.class);
+        when(ex.getPropertyName()).thenReturn("price");
+        when(ex.getValue()).thenReturn("abc");
+        when(ex.getRequiredType()).thenReturn(null);
+
+        WebRequest request = mock(WebRequest.class);
+
+        assertThrows(NullPointerException.class, () ->
+                handler.handleTypeMismatch(ex, new HttpHeaders(), HttpStatus.BAD_REQUEST, request)
+        );
+    }
+
 }
