54f92af24ec41fb35b1a86648721b4139539436b
lorenus-dev <lorena.sanchis@gft.com>
2025-05-22 16:57:47 +0200

    W2025ORDER-32: comprobar que funciona build en github

--- src/test/java/com/gft/orders/OrdersApplicationTests.java
+++ src/test/java/com/gft/orders/OrdersApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 @ActiveProfiles("test")
 class OrdersApplicationTests {
 
