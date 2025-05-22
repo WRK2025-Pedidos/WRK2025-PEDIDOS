1ef0c7eb6ae8ad5b045cab38723ae83d4beb0645
lorenus-dev <lorena.sanchis@gft.com>
2025-05-22 08:58:51 +0200

    W2025ORDER-32: OrderMapper interface created

--- src/main/java/com/gft/orders/application/mapper/OrderMapper.java
+++ src/main/java/com/gft/orders/application/mapper/OrderMapper.java
@@ -3,13 +3,12 @@ package com.gft.orders.application.mapper;
 import com.gft.orders.domain.model.entity.Order;
 import com.gft.orders.infraestructure.persistence.OrderJPAEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring",
         uses = {OrderLineMapper.class})
 public interface OrderMapper {
-
-    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
 
     Order toDomain(OrderRequest request);
     OrderResponse toResponse(Order domain);
