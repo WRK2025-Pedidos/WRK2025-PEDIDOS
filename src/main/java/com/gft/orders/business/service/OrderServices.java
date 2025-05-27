3c1670eb745f856630499079cf4e7e13d77821f2
lorenus-dev <lorena.sanchis@gft.com>
2025-05-27 16:39:36 +0200

    W2025ORDER-38: return line dto created

--- src/main/java/com/gft/orders/business/service/OrderServices.java
+++ src/main/java/com/gft/orders/business/service/OrderServices.java
@@ -1,8 +1,10 @@
 package com.gft.orders.business.service;
 
+import com.gft.orders.business.model.DTO.ReturnLineDTO;
 import com.gft.orders.business.model.Order;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +15,6 @@ public interface OrderServices {
     Optional<Order> findOrderById(UUID id);
 
     List<Order> findAllOrders();
+
+    UUID createOrderReturn(UUID orderId, Map<UUID, ReturnLineDTO> orderReturnLines);
 }
