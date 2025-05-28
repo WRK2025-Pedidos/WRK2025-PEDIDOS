3e8421fbbb812aa0a6ebe7c1f80f861fe3be6924
lorenus-dev <lorena.sanchis@gft.com>
2025-05-28 16:27:54 +0200

    W2025ORDER-38: return service and repository ok

--- src/main/java/com/gft/orders/business/service/OrderServices.java
+++ src/main/java/com/gft/orders/business/service/OrderServices.java
@@ -1,12 +1,9 @@
 package com.gft.orders.business.service;
 
-import com.gft.orders.business.model.DTO.ReturnLineDTO;
 import com.gft.orders.business.model.Order;
-import com.gft.orders.business.model.OrderLine;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,13 +15,5 @@ public interface OrderServices {
 
     List<Order> findAllOrders();
 
-    UUID createOrderReturn(UUID orderId, Map<UUID, ReturnLineDTO> orderReturnLines);
-
-    BigDecimal calculateLineTotal(OrderLine orderLine);
-
-    BigDecimal calculateLinesTotal(List<OrderLine> orderLines);
-
-    boolean enoughProductQuantity(UUID orderLine, BigDecimal quantity);
-
-    BigDecimal calculateReturnTotal(BigDecimal totalOrderLines, double countryTax, double paymentTax);
+    BigDecimal createOrderReturn(UUID orderId);
 }
--- src/main/java/com/gft/orders/business/service/impl/OrderServiceImpl.java
+++ src/main/java/com/gft/orders/business/service/impl/OrderServiceImpl.java
@@ -1,14 +1,10 @@
 package com.gft.orders.business.service.impl;
 
 import com.gft.orders.business.mapper.OrderMapper;
-import com.gft.orders.business.model.DTO.ReturnLineDTO;
 import com.gft.orders.business.model.Order;
-import com.gft.orders.business.model.OrderLine;
 import com.gft.orders.business.service.OrderServices;
 import com.gft.orders.integration.model.OrderJPA;
-import com.gft.orders.integration.model.OrderLineJPA;
 import com.gft.orders.integration.repositories.OrderJPARepository;
-import com.gft.orders.integration.repositories.OrderLineJPARepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +17,10 @@ public class OrderServiceImpl implements OrderServices {
 
     private final OrderJPARepository orderJPARepository;
     private final OrderMapper orderMapper;
-    private final OrderLineJPARepository orderLineJPARepository;
 
-    public OrderServiceImpl(OrderJPARepository orderJPARepository, OrderMapper orderMapper, OrderLineJPARepository orderLineJPARepository) {
+    public OrderServiceImpl(OrderJPARepository orderJPARepository, OrderMapper orderMapper) {
         this.orderJPARepository = orderJPARepository;
         this.orderMapper = orderMapper;
-        this.orderLineJPARepository = orderLineJPARepository;
     }
 
     @Override
@@ -50,89 +44,20 @@ public class OrderServiceImpl implements OrderServices {
     }
 
     @Override
-    public UUID createOrderReturn(UUID orderId, Map<UUID, ReturnLineDTO> orderReturnLines) {
+    @Transactional
+    public BigDecimal createOrderReturn(UUID orderId) {
 
         OrderJPA originalOrder = orderJPARepository.findById(orderId)
                                                     .orElseThrow(() -> new RuntimeException("Original order not found"));
 
-        if(orderJPARepository.idDateBeforeThirtyDays(orderId)) {
+        if(orderJPARepository.idDateBeforeThirtyDays(orderId, LocalDateTime.now())) {
             throw new RuntimeException("Return period exceeded");
         }
 
-        OrderJPA baseOrder = orderJPARepository.isFirstReturn(orderId)
-                            ? originalOrder
-                            : orderJPARepository.findLastReturn(orderId);
+        originalOrder.setOrderReturn(true);
 
-        for (Map.Entry<UUID, ReturnLineDTO> entry : orderReturnLines.entrySet()) {
-            UUID orderLineId = entry.getKey();
-            Integer quantityToReturn = entry.getValue().returnLines().get(orderLineId);
+        orderJPARepository.save(originalOrder);
 
-            if(!orderLineJPARepository.enoughProductQuantity(orderLineId, quantityToReturn)) {
-                throw new RuntimeException("Invalid quantity to return");
-            }
-        }
-
-        List<OrderLine> returnLines = new ArrayList<>();
-
-        for (Map.Entry<UUID, ReturnLineDTO> entry : orderReturnLines.entrySet()) {
-
-            UUID orderLineId = entry.getKey();
-            Integer quantityToReturn = entry.getValue().returnLines().get(orderLineId);
-
-            OrderLineJPA originalLine = orderLineJPARepository.findById(orderLineId)
-                    .orElseThrow(() -> new RuntimeException("Order line not found"));
-
-            OrderLine returnLine = new OrderLine(
-                    originalLine.getProduct(),
-                    quantityToReturn,
-                    originalLine.getLineWeight(),
-                    originalLine.getProductPrice(),
-                    originalLine.getProductPrice().multiply(BigDecimal.valueOf(quantityToReturn))
-            );
-
-            returnLines.add(returnLine);
-        }
-
-        Order returnOrder = new Order();
-        returnOrder.setId(UUID.randomUUID());
-        returnOrder.setCartId(baseOrder.getCartId());
-        returnOrder.setCreationDate(LocalDateTime.now());
-        returnOrder.setTotalPrice(calculateReturnTotal(calculateLinesTotal(returnLines), originalOrder.getCountryTax(), originalOrder.getPaymentMethod()));
-        returnOrder.setCountryTax(originalOrder.getCountryTax());
-        returnOrder.setPaymentMethod(originalOrder.getPaymentMethod());
-        returnOrder.setOrderReturn(true);
-        returnOrder.setParentOrderId(orderId);
-        returnOrder.setOrderLines(returnLines);
-
-        OrderJPA returnOrderJPA = orderMapper.toOrderJPA(returnOrder);
-        return orderJPARepository.save(returnOrderJPA).getId();
-
+        return originalOrder.getTotalPrice().negate();
     }
-
-    @Override
-    public BigDecimal calculateLineTotal(OrderLine orderLine) {
-        return orderLine.getProductPrice().multiply(BigDecimal.valueOf(orderLine.getQuantity()));
-    }
-
-    @Override
-    public BigDecimal calculateLinesTotal(List<OrderLine> orderLines) {
-
-        return orderLines.stream()
-                  .map((this::calculateLineTotal))
-                   .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
-    @Override
-    public boolean enoughProductQuantity(UUID orderLine, BigDecimal quantity) {
-        return false;
-    }
-
-    @Override
-    public BigDecimal calculateReturnTotal(BigDecimal totalOrderLines, double countryTax, double paymentTax) {
-
-        BigDecimal multiplier = BigDecimal.valueOf(1 + countryTax + paymentTax);
-
-        return totalOrderLines.multiply(multiplier);
-    }
-
 }
