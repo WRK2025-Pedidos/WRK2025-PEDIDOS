1bada41c1f9a4046acd4d43dd6b018ac18708bf3
lorenus-dev <lorena.sanchis@gft.com>
2025-05-30 10:15:26 +0200

    W2025ORDER-42: interface and impl created

---
+++ src/main/java/com/gft/orders/business/service/impl/ExternalApiServiceImpl.java
@@ -0,0 +1,23 @@
+package com.gft.orders.business.service.impl;
+
+import com.gft.orders.business.service.ExternalApiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ExternalApiServiceImpl implements ExternalApiService {
+
+    private final RestTemplate restTemplate;
+    private final String externalApiBaseUrl;
+
+    public ExternalApiServiceImpl(RestTemplate restTemplate, @Value("${external.api.base.url}") String externalApiBaseUrl) {
+        this.restTemplate = restTemplate;
+        this.externalApiBaseUrl = externalApiBaseUrl;
+    }
+
+    @Override
+    public String getDataFromExternalApi() {
+        return "Calling external API from: " + externalApiBaseUrl;
+    }
+}
