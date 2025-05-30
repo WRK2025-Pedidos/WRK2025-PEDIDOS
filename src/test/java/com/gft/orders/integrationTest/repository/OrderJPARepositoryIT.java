0b4a6f2df63c0ac25a19c71e19dc4f67cc6a4cfb
lorenus-dev <lorena.sanchis@gft.com>
2025-05-30 08:45:51 +0200

    Order repository tests green

---
+++ src/test/java/com/gft/orders/integrationTest/repository/OrderJPARepositoryIT.java
@@ -0,0 +1,46 @@
+package com.gft.orders.integrationTest.repository;
+
+import com.gft.orders.integration.model.OrderJPA;
+import com.gft.orders.integration.repositories.OrderJPARepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@Sql(scripts = {"/db/data.sql", "/db/schema.sql"})
+public class OrderJPARepositoryIT {
+
+    @Autowired
+    OrderJPARepository repository;
+
+    @Test
+    void findOrderByIdTest(){
+
+        UUID id = UUID.fromString("11111111-1111-1111-1111-111111111111");
+
+        Optional<OrderJPA> order = repository.findById(id);
+
+        assertThat(order.isPresent());
+        assertThat(order.get().getId()).isEqualTo(id);
+
+    }
+
+    @Test
+    void idDateBeforeThirtyDaysTest(){
+
+        UUID id = UUID.fromString("11111111-1111-1111-1111-111111111111");
+
+        Boolean response = repository.idDateBeforeThirtyDays(id, LocalDateTime.now());
+
+        assertEquals(Boolean.TRUE, response);
+
+    }
+}
