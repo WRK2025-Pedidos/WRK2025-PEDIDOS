6c0727339aecef20bb9fc292a24acc764134363d
Kathy <catherine.alventos@gft.com>
2025-05-30 13:19:41 +0200

    W2025ORDER-45: Create and implement RabbitMQ

---
+++ src/main/java/com/gft/orders/messaging/config/RabbitConfig.java
@@ -0,0 +1,33 @@
+package com.gft.orders.messaging.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String EXCHANGE_PRODUCT = "order";
+
+    @Bean
+    public TopicExchange productExchange() {
+        return new TopicExchange(EXCHANGE_PRODUCT);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+}
---
+++ src/main/java/com/gft/orders/messaging/producer/StockNoticationChange.java
@@ -0,0 +1,5 @@
+package com.gft.orders.messaging.producer;
+
+public class StockNoticationChange {
+
+}
