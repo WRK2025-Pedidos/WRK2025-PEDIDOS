293eeb1dfb54a73302b2d93dfc0a62f50543caea
lorenus-dev <lorena.sanchis@gft.com>
2025-06-02 09:20:51 +0200

    W2025ORDER-43: offerDto and start of offerService

--- src/main/java/com/gft/orders/offer/OfferService.java
+++ src/main/java/com/gft/orders/offer/OfferService.java
@@ -1,4 +1,31 @@
 package com.gft.orders.offer;
 
+import com.gft.orders.offer.client.ProductServiceClient;
+import com.gft.orders.offer.client.dto.OfferDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class OfferService {
+
+    private final ProductServiceClient productServiceClient;
+
+    public List<Long> getApplicableOffers(Map<Long, Integer> productQuantities) {
+
+        Set<Long> productCategories = productServiceClient.getFamiliesCategories(productQuantities.keySet());
+
+        Map<Long, List<OfferDto>> offersByCategory = productServiceClient.getOffersByFamilies(productCategories);
+
+        return filterOffers(productQuantities, offersByCategory);
+    }
+
+    private List<Long> filterOffers(Map<Long, Integer> productQuantities, Map<Long, List<OfferDto>> offersByCategory) {
+
+        List<Long> offers = new ArrayList<>();
+
+
+
+    }
 }
--- src/main/java/com/gft/orders/offer/client/dto/OfferDto.java
+++ src/main/java/com/gft/orders/offer/client/dto/OfferDto.java
@@ -1,4 +1,14 @@
 package com.gft.orders.offer.client.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class OfferDto {
+
+    private Long id;
+    private String category;
+    private int quantity;
+
 }
