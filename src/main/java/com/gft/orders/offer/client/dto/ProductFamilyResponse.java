fed951377d1c6a6778aa953b4c8bf19cebbf3d97
lorenus-dev <lorena.sanchis@gft.com>
2025-06-02 09:24:09 +0200

    W2025ORDER-43: productFamiliResponseDto

--- src/main/java/com/gft/orders/offer/client/dto/ProductFamilyResponse.java
+++ src/main/java/com/gft/orders/offer/client/dto/ProductFamilyResponse.java
@@ -1,4 +1,11 @@
 package com.gft.orders.offer.client.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductFamilyResponse {
+    private Long productId;
+    private Long familyId;
 }
