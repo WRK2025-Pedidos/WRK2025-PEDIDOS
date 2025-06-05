781f27d636e8390960e79f835a718ff56fd85ed0
lorenus-dev <lorena.sanchis@gft.com>
2025-06-05 12:49:46 +0200

    actualizacion a api/v1

--- src/main/java/com/gft/orders/offer/OfferController.java
+++ src/main/java/com/gft/orders/offer/OfferController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/offers")
+@RequestMapping("/api/v1/offers")
 public class OfferController {
 
     private final OfferService offerService;
