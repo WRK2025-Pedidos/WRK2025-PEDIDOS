0837ef839bd5603d1ead25cf8559cc1872b8321a
lorenus-dev <lorena.sanchis@gft.com>
2025-06-05 20:56:43 +0200

    Matame camion (#88)
    * cambio de key long a string
    
    * quitamos credenciales porque carles tiene el portatil en la taquilla y no puede levantar el rabbit :)
    
    * a ver si esto funciona
    
    * vamos a ver si el nuestro va
    
    * record CategoriesRequest creado
    
    * faltaba el test
    
    * cambio de localhost a ip real para rabbitmq
    
    * cambiamos a post

--- src/test/java/com/gft/orders/unittest/offers/ProductServiceClientTest.java
+++ src/test/java/com/gft/orders/unittest/offers/ProductServiceClientTest.java
@@ -1,7 +1,6 @@
 package com.gft.orders.unittest.offers;
 
 import com.gft.orders.offer.client.ProductServiceClient;
-import com.gft.orders.offer.client.dto.CategoriesRequest;
 import com.gft.orders.offer.client.dto.OfferDTO;
 import com.gft.orders.offer.client.dto.ProductDTO;
 import com.gft.orders.offer.client.exception.ProductServiceException;
